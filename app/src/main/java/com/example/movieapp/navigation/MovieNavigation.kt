6507d2d63c913e0b7fd3eca431a92df6fcad36c8
muhith7 <abdulmuhithnawawi@gmail.com>
2024-02-27 11:15:43 +0700

    Setting up the Navigation Structure

---
+++ app/src/main/java/com/example/movieapp/navigation/MovieNavigation.kt
@@ -0,0 +1,25 @@
+package com.example.movieapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.movieapp.screens.details.DetailScreen
+import com.example.movieapp.screens.home.HomeScreen
+
+@Composable
+fun MovieNavigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = MovieScreens.HomeScreen.name
+    ) {
+        composable(MovieScreens.HomeScreen.name) {
+            //here we pass where this should lead us
+            HomeScreen(navController = navController)
+        }
+        composable(MovieScreens.DetailsScreen.name) {
+            DetailScreen(navController = navController)
+        }
+    }
+}
