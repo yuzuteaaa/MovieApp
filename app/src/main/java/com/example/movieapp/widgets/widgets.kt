4daea499d225991f95e570a565f6c760ce0c7ad1
muhith7 <abdulmuhithnawawi@gmail.com>
2024-02-29 22:26:50 +0700

    Adding Movie Data class and Dummy Data

--- app/src/main/java/com/example/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/example/movieapp/widgets/widgets.kt
@@ -1,11 +1,7 @@
-package com.example.movieapp
+package com.example.movieapp.widgets
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -24,36 +20,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.movieapp.model.Movie
-import com.example.movieapp.navigation.MovieNavigation
-import com.example.movieapp.ui.theme.MovieAppTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MyApp {
-                MovieNavigation()
+@Composable
+fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp)
+            .clickable {
+                onItemClick(movie.id)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                shadowElevation = 4.dp
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountBox,
+                    contentDescription = "Movie Image"
+                )
             }
+            Text(text = movie.title)
         }
-    }
-}
-
-
-@Composable
-fun MyApp(content: @Composable (PaddingValues) -> Unit) {
-    MovieAppTheme {
-        MovieNavigation()
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApp {
-        MovieNavigation()
     }
 }
